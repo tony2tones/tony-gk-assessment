b1e4c6a3efaf3482447b6e8e5a89ab3581b524ab
anthony <anthony@globalkinetic.com>
2025-02-04 18:46:38 +0200

    removed inline error message

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -19,7 +19,6 @@ const SignIn = () => {
   const router = useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
 const onSubmit = async (e:FormEvent) => {
   e.preventDefault();
   try {
@@ -28,8 +27,6 @@ const onSubmit = async (e:FormEvent) => {
     router.push('/dashboard');
   } catch (err) {
     const firebaseErrors = err as FirebaseError
-    console.log(firebaseErrors.message)
-    setErrorMessage(firebaseErrors.message)
     toast.error(firebaseErrors.message);
   }
 }
@@ -64,7 +61,6 @@ const navToSignUp =() => {
           onChange={(e) => setPassword(e.target.value)}
           className='w-full p-3 mb-4 bg-slate-100 rounded outline-none text-slate-950 placeholder-gray-500'
           />
-          {errorMessage && <p className='text-red-700 font-semibold'>{errorMessage}</p>}
           <div className='flex justify-between'>
       <button type='submit' className='bg-sky-400'>Sign in</button>
         <button type='button' onClick={navToSignUp}>Need to signed up?</button>
