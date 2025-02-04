f4c0898aa3da4b4acb9ef6088d8430d4312b538b
anthony <anthony@globalkinetic.com>
2025-02-04 22:30:21 +0200

    minor updates

--- app/components/userProfile.tsx
+++ app/components/userProfile.tsx
@@ -71,8 +71,7 @@ export default function ProfilePage() {
   if (loading) {
     return (
       <div className="min-h-screen flex items-center justify-center">
-        <Loader2 />
-      </div>
+        </div>
     );
   }
 
